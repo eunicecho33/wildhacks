6bb8da1e47bd10345be2ea0a8b35e7142d7e5d66
eunicecho33 <147003259+eunicecho33@users.noreply.github.com>
2024-04-06 20:45:39 -0500

    yes

--- aggregate.py
+++ aggregate.py
@@ -1,3 +1,1 @@
 import pandas as pd
-
-# given factor1, factor2, factor3 wherein factor1 is ranked first, etc
---
+++ calculate_factors.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+cps = pd.read_csv('cps_sy1819.csv')
+
+print(cps)
