1bc6ab9f2ceaa51e231f12f86e9d007563eb2787
Eshan Haq <eshanhaq4@gmail.com>
2024-04-07 07:23:54 -0500

    edit

--- vite-project/src/Ending.jsx
+++ vite-project/src/Ending.jsx
@@ -4,7 +4,7 @@ function Ending() {
   
     return(
         <>
-        <img src={f1} width={1100} height = {700} margin={100} alt="F1 Redbull Image" style = {{float: 'right', marginLeft: '20px', marginRight: '10px', marginTop: '50px'}} /> 
+        <img src={f1} width={1100} height = {700} margin={100} alt="F1 Redbull Image" style = {{float: 'right', marginLeft: '20px', marginRight: '10px', marginTop: '40px'}} /> 
         <h1>1. </h1>
             <><br /> </>
             <><br /> </>
