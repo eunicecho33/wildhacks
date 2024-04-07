fdd355d6f8a6705104e61210a166e1b1616feba2
Eshan Haq <eshanhaq4@gmail.com>
2024-04-07 03:32:58 -0500

    main

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -9,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Message />
-    <SecondApp />
     <App />
    
   </React.StrictMode>,
