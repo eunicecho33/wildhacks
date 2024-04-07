1457e6aab84cd182e31b0376cbe875fc18dcf996
billybobjoetoe <158264912+billybobjoetoe@users.noreply.github.com>
2024-04-07 01:45:00 -0500

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href= "/" className="site-title">Your Search</a>
+        <ul>
+            <li>
+                <a href= "/about">About</a>
+            </li>
+        </ul>
+    </nav>
+}
