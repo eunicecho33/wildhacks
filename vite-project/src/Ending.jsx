ef4d8d029a7d7c3e70cb39ca6f176f6f5008dcea
Eshan Haq <eshanhaq4@gmail.com>
2024-04-07 07:11:56 -0500

    attempt end

---
+++ vite-project/src/Ending.jsx
@@ -0,0 +1,38 @@
+import f1 from './images/f1.png';
+
+function Ending() {
+  
+    return(
+        <>
+        <img src={f1} width={400} height = {200} margin={100} alt="F1 Redbull Image" /> 
+        <h1>1. </h1>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+        <h1>2. </h1>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+        <h1>3. </h1>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+        <h1>4. </h1>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+        <h1>5. </h1>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+            <><br /> </>
+        </>
+    );
+   
+    
+}
+export default Ending;
